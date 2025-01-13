34fbd398bd77559d4e94baab59f1d83812162cf0
nova34tkj4 <novahariyakusuma@gmail.com>
2025-01-13 20:54:50 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project = "tanooki-staging"
+  region  = "us-central1"  # Replace with the desired GCP region
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-unique-bucket-name"  # Replace with your unique bucket name
+  location = "US"  # Specify the bucket's location (e.g., "US" or "EU")
+
+  lifecycle {
+    prevent_destroy = true  # Optional: Prevent bucket from being accidentally destroyed
+  }
+}
